b917965e78f7d60029313bf76e0b9ac44c58ad05
Aayushman4725 <aayushmannp@gmail.com>
2025-01-12 17:21:14 +0545

    merged url paths

--- backend/authenticate/urls.py
+++ backend/authenticate/urls.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-
-
-urlpatterns= [
-    path('register/', UserRegistrationView.as_view(), name='reguster'),
-
-   
-
-]
-
-
-=======
->>>>>>> 6269349d53518f30fad4bffa16e18e5b0fc9c6fd
 from django.urls import path,include
 from .views import *
 urlpatterns = [
