870557c7353cfb88688ef1e6659498cdc70752be
Aayush pant <aayush.211702@ncit.edu.np>
2025-01-12 10:53:58 +0545

    new commit

--- shahayak/lib/screens/HomeScreen.dart
+++ shahayak/lib/screens/HomeScreen.dart
@@ -2,48 +2,103 @@ import 'package:flutter/material.dart';
 import 'User.dart';
 import 'post.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shahayak/models/category_model.dart';
 
-class Homescreen extends StatelessWidget {
+class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  List<CategoryModel> categories = [];
+  void _getCategories() {
+    categories = CategoryModel.getCategories();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCategories();
+  }
 
   @override
   Widget build(BuildContext context) {
+    _getCategories();
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        leading: GestureDetector(
+      appBar: appBar(),
+    );
+  }
+
+  Container _SearchField() {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+            color: const Color(0xff1D1537).withOpacity(0.11),
+            blurRadius: 40,
+            spreadRadius: 0),
+      ]),
+      child: Row(
+        children: [
+          SvgPicture.asset(
+            'assets/icons/Search.svg',
+          ),
+          const SizedBox(width: 10),
+          const Text(
+            'Search',
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Shahayak',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      leading: GestureDetector(
+        onTap: () {},
+        child: Container(
+          margin: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset('assets/icons/profile-svhrepo-com.svg',
+              height: 20, width: 20),
+        ),
+      ),
+      actions: [
+        GestureDetector(
           onTap: () {},
           child: Container(
-            margin: const EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
             alignment: Alignment.center,
+            width: 37,
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(10),
             ),
-            child: SvgPicture.asset(
-              'assets/icons/profile-svgrepo-com.svg',
-            ),
+            child: SvgPicture.asset('assets/icons/setting-svgrepo-com.svg',
+                height: 5, width: 5),
           ),
         ),
-        actions: [
-          GestureDetector(
-            onTap: () {},
-            child: Container(
-              margin: const EdgeInsets.all(10),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: SvgPicture.asset(
-                'assets/icons/setting-svgrepo-com.svg',
-              ),
-            ),
-          ),
-        ],
-        centerTitle: true,
-      ),
+      ],
+      centerTitle: true,
     );
   }
 }
