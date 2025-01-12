3d98ae3c49287a11ba1e623c9359a5cc6f56305e
Aayushman4725 <aayushmannp@gmail.com>
2025-01-12 15:19:10 +0545

    added models for category and product

--- backend/Product/models.py
+++ backend/Product/models.py
@@ -1,27 +1,36 @@
 from django.db import models
-from authenticate.models import User
 from django.conf import settings
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
-def get_category_choices():
-    return [(category.name.lower(), category.name) for category in Category.objects.all()]
 
 class Product(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     product_name = models.CharField(max_length=255)
     category = models.CharField(
         max_length=50,
-        choices=get_category_choices,  # Fetch choices dynamically
-        default='clothes'
+        choices=[]  
     )
     image = models.ImageField(default='images/', upload_to='images/product/')
     price = models.FloatField()
     purchase_date = models.CharField(max_length=255)
     product_condition = models.CharField(max_length=255)
     caption = models.CharField(max_length=255)
+
+    @staticmethod
+    def get_category_choices():
+        """Fetch all categories from the Category model."""
+        categories = Category.objects.all()
+        return [(category.name.lower(), category.name) for category in categories]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Dynamically set choices for the category field
+        self._meta.get_field('category').choices = self.get_category_choices()
+
+    def __str__(self):
+        return self.product_name
