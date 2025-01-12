98f412785707aef781d7b22424c4f2084dbb6237
Aayushman4725 <aayushmannp@gmail.com>
2025-01-12 12:55:01 +0545

    added registration serializer

--- backend/authenticate/serializers.py
+++ backend/authenticate/serializers.py
@@ -10,3 +10,22 @@ class UserLoginSerializer(serializers.ModelSerializer):
             'password',
         ]
         
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password2= serializers.CharField(style = {'input_type': 'password' }, write_only= True)
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'password', 'password2')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validate(self,attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        
+        if password != password2:
+            raise serializers.ValidationError("Password and Confirm password do not match")
+        return attrs 
+    
+    def create(self, validate_data):
+        return User.objects.create_user(**validate_data)
