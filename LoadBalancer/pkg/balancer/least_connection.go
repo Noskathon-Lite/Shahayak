af241c3e15ca4e1d3760cf442acbbce782b610d4
Aaryan Baral <aryanbaral1100@gmail.com>
2025-01-12 14:34:13 +0545

    getLeastConnectionServers

--- LoadBalancer/pkg/balancer/least_connection.go
+++ LoadBalancer/pkg/balancer/least_connection.go
@@ -14,3 +14,21 @@ type LeastConnection struct {
 	HealthCheckUrl string
 	port           string
 }
+
+func (lc *LeastConnection) GetServer() server.Server {
+	lc.mutex.Lock()
+	defer lc.mutex.Unlock()
+
+	var selectedServer server.Server
+	minConnections := int(^uint(0) >> 1) // Max int value
+
+	// Find the server with the least number of active connections
+	for _, server := range lc.Servers {
+		if server.GetConnections() < minConnections {
+			minConnections = server.GetConnections()
+			selectedServer = server
+		}
+	}
+	return selectedServer
+
+}
