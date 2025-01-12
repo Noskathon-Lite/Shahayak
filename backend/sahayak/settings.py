a99c4abb3edb78ae556553e302c23a1d1ce2b280
Aayushman4725 <aayushmannp@gmail.com>
2025-01-12 17:18:17 +0545

    added host

--- backend/sahayak/settings.py
+++ backend/sahayak/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wwkn49-(7#42%l18$ei1nb6llm%$x20@3fww=e0l(o&m&@^dn&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
