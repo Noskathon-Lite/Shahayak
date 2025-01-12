ac1731ac7a84cbb12605df1ecf35462be2a771ce
Aayushman4725 <aayushmannp@gmail.com>
2025-01-12 12:55:28 +0545

    added registration url

--- backend/authenticate/urls.py
+++ backend/authenticate/urls.py
@@ -3,4 +3,5 @@ from django.urls import path,include
 from .views import *
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login' ),
+    path('register/', UserRegistrationView.as_view(), name='register')
 ]
