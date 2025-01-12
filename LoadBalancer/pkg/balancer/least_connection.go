7af681ea82b5549139ec6145310f0d90db42cacb
Aaryan Baral <aryanbaral1100@gmail.com>
2025-01-12 14:33:41 +0545

    least connection struct

--- LoadBalancer/pkg/balancer/least_connection.go
+++ LoadBalancer/pkg/balancer/least_connection.go
@@ -1,1 +1,16 @@
 package balancer
+
+import (
+	"LoadBalancer/pkg/server"
+	"LoadBalancer/pkg/utils"
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+type LeastConnection struct {
+	Servers        []server.Server
+	mutex          sync.Mutex
+	HealthCheckUrl string
+	port           string
+}
