06f357f877fe1b46e28b4067f028590563ed501a
Shreya-shr <shreyabhantana1@gmail.com>
2025-01-12 10:57:06 +0545

    registration system

---
+++ backend/authenticate/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from account.views import UserRegistrationView
+
+urlpatterns= [
+    path('register/', UserRegistrationView.as_view(), name='reguster'),
+
+   
+
+    
+
+]
