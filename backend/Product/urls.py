b67ee1d85c14f5b628898c2d6bce9c384da3b590
Aayushman4725 <aayushmannp@gmail.com>
2025-01-12 16:13:18 +0545

    added url for delete, updating and fetching product

--- backend/Product/urls.py
+++ backend/Product/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
 from .views import *
 urlpatterns = [
-    path('product_list/', ProductListView.as_view(),name = 'ProductList')
+    path('product_list/', ProductListView.as_view(),name = 'ProductList'),
+    path('product_detail/<int:pk>', Product_DetailsView.as_view(),name = 'Product_Details'),
 ]
 
