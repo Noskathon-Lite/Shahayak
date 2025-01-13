46c16721786b6aa1cdff65c486d9e60c3a19e7c0
Aayushman4725 <aayushmannp@gmail.com>
2025-01-13 09:22:54 +0545

    added urls for like toggle

--- backend/Product/urls.py
+++ backend/Product/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('product_detail/<int:pk>', Product_DetailsView.as_view(),name = 'Product_Details'),
     path('comment_list/', CommentListView.as_view(),name = 'CommentList'),
     path('comment_update/<int:pk>', CommentUpdateView.as_view(), name='Comment_Update'),
+    path('products/<int:pk>/like/', LikeToggleView.as_view(), name='like-toggle'),
+
 ]
 
